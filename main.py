9c21079c3657eb449163fec3a63d8e722de8130c
song23jihye <97861641+song23jihye@users.noreply.github.com>
2023-11-06 14:20:35 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+# from langchain.llms import OpenAI 
+# llm=OpenAI() #이어 작성
+# result = llm.predict('내가 좋아하는 음식은')
+# print(result)
+
+from langchain.chat_models import ChatOpenAI
+import streamlit as st
+import time
+
+chat_model=ChatOpenAI() #질문<->답변
+
+st.title('클리블랜드 심장질환 진단')
+
+content = st.text_input("진단사항에 대해 말씀해주세요.")
+
+st.button("Reset", type="primary")
+if st.button('시 작성 요청하기'):
+    with st.spinner('Wait for it...'):
+        result = chat_model.predict(content+"에 대한 시를 써줘")
+        st.write('result')
