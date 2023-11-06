6e330c8d146913287832745fe972b9abde802da8
song23jihye <97861641+song23jihye@users.noreply.github.com>
2023-11-06 16:25:14 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ chat_model=ChatOpenAI() #대화주고받기
 
 st.title('클리블랜드 심장질환 진단')
 
-content = st.text_input("진단사항에 대해 말씀해주세요.")
+content = st.text_input(""진단사항에 대해 말씀해주세요."")
 
 st.button("Reset", type="primary")
-if st.button('시 작성 요청하기'):
+if st.button('요청하기'):
     with st.spinner('Wait for it...'):
-        result = chat_model.predict(content+"에 대한 시를 써줘")
+        result = chat_model.predict(content+"의 종류 한가지만 말해줘")
         st.write(result)
